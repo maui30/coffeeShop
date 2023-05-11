93986b0595d76887fc5679b9d9cafadae88deade
maui30 <103097103+maui30@users.noreply.github.com>
2023-05-11 22:40:06 +0800

    remove some part

--- script.js
+++ script.js
@@ -1,36 +1,29 @@
 hamburger = document.querySelector('#menu-btn');
 
 hamburger.onclick = function () {
-    navbar = document.querySelector('.navbar');
+    let navbar = document.querySelector('.navbar');
     navbar.classList.toggle("active");
 }
 
+let navbarS = document.querySelector("header");
 
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
-
-// Get the navbar
-let navbar = document.querySelector("header");
-
-// Get the offset position of the navbar
-let sticky = navbar.offsetTop;
+let sticky = navbarS.offsetTop;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
   if (window.scrollY >= sticky) {
-    navbar.classList.add("sticky")
+    navbarS.classList.add("sticky")
   } else {
-    navbar.classList.remove("sticky");
+    navbarS.classList.remove("sticky");
   }
 }
 
-// Add a scroll event listener
 window.addEventListener("scroll", function() {
   // Check if the user has scrolled past a certain threshold
   if (window.scrollY >= 100) {
     myFunction();
   } else {
-    navbar.classList.remove("sticky");
+    navbarS.classList.remove("sticky");
   }
 });
 
